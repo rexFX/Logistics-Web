f86b8303e9142864f329197caa8ebda459a27c39
Shivam Karn <shivam3511@gmail.com>
2023-06-09 19:07:07 +0530

    add useEffect to start the onrender server upon initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+	useEffect(() => {
+		axios.get(import.meta.env.VITE_BACKEND).then((res) => console.log(res.data));
+	}, []);
+
 	return (
 		<div className="h-screen w-screen bg-bgImage bg-cover bg-center flex justify-end">
 			<Outlet />
